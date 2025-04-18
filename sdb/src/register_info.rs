ba1a2d460a1ed07b577f6619aee70ff7467bbc39
Shane Lillie <ignignokterr@gmail.com>
2025-04-18 08:39:07 -0700

    All of the GPRs

--- sdb/src/register_info.rs
+++ sdb/src/register_info.rs
@@ -1,9 +1,9 @@
 #![allow(dead_code)]
-#![allow(unused_macros)]
 #![allow(non_camel_case_types)]
 
 #[derive(Debug)]
 pub enum RegisterId {
+    // 64-bit GPRs
     rax,
     rdx,
     rcx,
@@ -28,7 +28,69 @@ pub enum RegisterId {
     ss,
     ds,
     es,
+
+    // special ptrace value
     orig_rax,
+
+    // 32-bit subregisters
+    eax,
+    edx,
+    ecx,
+    ebx,
+    esi,
+    edi,
+    ebp,
+    esp,
+    r8d,
+    r9d,
+    r10d,
+    r11d,
+    r12d,
+    r13d,
+    r14d,
+    r15d,
+
+    // 16-bit subregisters
+    ax,
+    dx,
+    cx,
+    bx,
+    si,
+    di,
+    bp,
+    sp,
+    r8w,
+    r9w,
+    r10w,
+    r11w,
+    r12w,
+    r13w,
+    r14w,
+    r15w,
+
+    // high 8-bit subregisters
+    ah,
+    dh,
+    ch,
+    bh,
+
+    // low 8-bit subregisters
+    al,
+    dl,
+    cl,
+    bl,
+    sil,
+    dil,
+    bpl,
+    spl,
+    r8b,
+    r9b,
+    r10b,
+    r11b,
+    r12b,
+    r13b,
+    r14b,
+    r15b,
 }
 
 #[derive(Debug)]
@@ -58,7 +120,9 @@ pub struct RegisterInfo {
     format: RegisterFormat,
 }
 
-// TODO: https://doc.rust-lang.org/nightly/core/mem/macro.offset_of.html
+// TODO:
+// https://doc.rust-lang.org/std/mem/macro.offset_of.html
+// https://doc.rust-lang.org/std/mem/fn.size_of.html
 
 macro_rules! define_gpr_64 {
     ($name:ident, $dwarf_id:literal) => {
@@ -75,7 +139,7 @@ macro_rules! define_gpr_64 {
 }
 
 macro_rules! define_gpr_32 {
-    ($name:ident, $dwarf_id:literal) => {
+    ($name:ident, $super:ident) => {
         RegisterInfo {
             id: RegisterId::$name,
             name: "$name",
@@ -89,7 +153,7 @@ macro_rules! define_gpr_32 {
 }
 
 macro_rules! define_gpr_16 {
-    ($name:ident, $dwarf_id:literal) => {
+    ($name:ident, $super:ident) => {
         RegisterInfo {
             id: RegisterId::$name,
             name: "$name",
@@ -131,6 +195,7 @@ macro_rules! define_gpr_8l {
 }
 
 pub const REGISTER_INFOS: &[RegisterInfo] = &[
+    // 64-bit GPRs
     define_gpr_64!(rax, 0),
     define_gpr_64!(rdx, 1),
     define_gpr_64!(rcx, 2),
@@ -155,5 +220,62 @@ pub const REGISTER_INFOS: &[RegisterInfo] = &[
     define_gpr_64!(ss, 52),
     define_gpr_64!(ds, 53),
     define_gpr_64!(es, 50),
+    // special ptrace value
     define_gpr_64!(orig_rax, -1),
+    // 32-bit subregisters
+    define_gpr_32!(eax, rax),
+    define_gpr_32!(edx, rdx),
+    define_gpr_32!(ecx, rcx),
+    define_gpr_32!(ebx, rbx),
+    define_gpr_32!(esi, rsi),
+    define_gpr_32!(edi, rdi),
+    define_gpr_32!(ebp, rbp),
+    define_gpr_32!(esp, rsp),
+    define_gpr_32!(r8d, r8),
+    define_gpr_32!(r9d, r9),
+    define_gpr_32!(r10d, r10),
+    define_gpr_32!(r11d, r11),
+    define_gpr_32!(r12d, r12),
+    define_gpr_32!(r13d, r13),
+    define_gpr_32!(r14d, r14),
+    define_gpr_32!(r15d, r15),
+    // 16-bit subregisters
+    define_gpr_16!(ax, rax),
+    define_gpr_16!(dx, rdx),
+    define_gpr_16!(cx, rcx),
+    define_gpr_16!(bx, rbx),
+    define_gpr_16!(si, rsi),
+    define_gpr_16!(di, rdi),
+    define_gpr_16!(bp, rbp),
+    define_gpr_16!(sp, rsp),
+    define_gpr_16!(r8w, r8),
+    define_gpr_16!(r9w, r9),
+    define_gpr_16!(r10w, r10),
+    define_gpr_16!(r11w, r11),
+    define_gpr_16!(r12w, r12),
+    define_gpr_16!(r13w, r13),
+    define_gpr_16!(r14w, r14),
+    define_gpr_16!(r15w, r15),
+    // high 8-bit subregisters
+    define_gpr_8h!(ah, rax),
+    define_gpr_8h!(dh, rdx),
+    define_gpr_8h!(ch, rcx),
+    define_gpr_8h!(bh, rbx),
+    // low 8-bit subregisters
+    define_gpr_8l!(al, rax),
+    define_gpr_8l!(dl, rdx),
+    define_gpr_8l!(cl, rcx),
+    define_gpr_8l!(bl, rbx),
+    define_gpr_8l!(sil, rsi),
+    define_gpr_8l!(dil, rdi),
+    define_gpr_8l!(bpl, rbp),
+    define_gpr_8l!(spl, rsp),
+    define_gpr_8l!(r8b, r8),
+    define_gpr_8l!(r9b, r9),
+    define_gpr_8l!(r10b, r10),
+    define_gpr_8l!(r11b, r11),
+    define_gpr_8l!(r12b, r12),
+    define_gpr_8l!(r13b, r13),
+    define_gpr_8l!(r14b, r14),
+    define_gpr_8l!(r15b, r15),
 ];
